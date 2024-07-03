a25732705809718cccb4f3e8fc3beb66c0ca17bb
Xiaotian Li <xiaotian.li@lapisit.com.au>
2024-07-03 15:41:31 +1000

    init proj

---
+++ src/main/kotlin/org/worker996/kotlinsprintbootpd/KotlinSprintbootPdApplication.kt
@@ -0,0 +1,11 @@
+package org.worker996.kotlinsprintbootpd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSprintbootPdApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSprintbootPdApplication>(*args)
+}
---
+++ src/test/kotlin/org/worker996/kotlinsprintbootpd/KotlinSprintbootPdApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.worker996.kotlinsprintbootpd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSprintbootPdApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
